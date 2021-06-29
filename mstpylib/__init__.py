53cbf862390bc775f6a9b90eae7b06f1e8095fd6
Rosas, Arturo <arturo@mst.edu>
2021-06-29 15:27:41 -0500

    doc update

--- mstpylib/__init__.py
+++ mstpylib/__init__.py
@@ -1,2 +1,8 @@
+# Begin-Doc
+# Name: __init__.py
+# Type: module
+# Description: central entrypoint into the mstpylib module, this file is mainly used for exposing some lower level functions directly into the mstpylib.* scope
+#    i.e. mstpylib.setuid.setuid -> mstpylib.setuid
+# End-Doc
 from mstpylib.setuid import *
 from mstpylib.local_env import *
